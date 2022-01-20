a4c4a58f2a740974c32a087b5be11a46a8b722d9
jonasaky <luis.garcia@wizeline.com>
2022-01-19 21:15:10 -0600

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Footer() {
   return (
-    <div style={{ position: 'fixed', bottom: 30 }}>
+    <div style={{ position: 'absolute', bottom: 2, left: 2 }}>
       <Typography variant="body2" color="text.secondary" align="center">
         {'Copyright © '}
         <a color="inherit" href="/">
