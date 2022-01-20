ac8035b71be2eb3b55c7108aa9638dac9b572612
jonasaky <luis.garcia@wizeline.com>
2022-01-19 21:17:56 -0600

    revert footer change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import { Typography } from "@mui/material";
 
 export default function Footer() {
   return (
-    <div style={{ position: 'absolute', bottom: 2, left: 2 }}>
+    <div>
       <Typography variant="body2" color="text.secondary" align="center">
         {'Copyright © '}
         <a color="inherit" href="/">
