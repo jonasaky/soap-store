77e75a6616154dce02badadb2d2c48ee6743fccc
jonasaky <luis.garcia@wizeline.com>
2022-01-19 22:31:19 -0600

    added alert to Buy click button

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
+import { Alert, Button } from '@mui/material';
 import data from '../../data.json';
 import './Card.css';
 
@@ -9,23 +9,33 @@ export default function Card() {
   const { id } = useParams();
   const item = data.items.find(i => i.id === parseInt(id));
   const formatter = Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+  const [showAlert, setShowAlert] = React.useState(false);
+  const visibility = showAlert ? 'visible' : 'hidden';
+
+  const buyItem = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 2000);
+  };
 
   return (
     item 
     ?
-    <div className='container'>
-        <img className='card-img' src={item.img} alt={item.name} style={{ marginRight: 25, maxWidth: 300, objectFit: 'cover' }} loading='lazy'/>
-        <div style={{ textAlign: 'initial' }}>
-          <Typography gutterBottom variant="h5" component="div">
-            { item.name }
-          </Typography>
-          <Typography variant="body1" color="text.secondary">
-            {item.description}
-          </Typography>
-          <br />
-          <Button variant='contained' >Buy {formatter.format(item.price)}</Button>
-        </div>
-    </div>
+    <>
+      <div className='container'>
+          <img className='card-img' src={item.img} alt={item.name} style={{ marginRight: 25, maxWidth: 300, objectFit: 'cover' }} loading='lazy'/>
+          <div style={{ textAlign: 'initial' }}>
+            <Typography gutterBottom variant="h5" component="div">
+              { item.name }
+            </Typography>
+            <Typography variant="body1" color="text.secondary">
+              {item.description}
+            </Typography>
+            <br />
+            <Button variant='contained' onClick={() => buyItem()} >Buy {formatter.format(item.price)}</Button>
+          </div>
+      </div>
+      <Alert severity="success" className='alert' style={{ visibility }}>Thanks for your purchase!</Alert>
+    </>
     : <div style={{ display: 'flex', margin: 25 }}>Product Not Found</div>
   );
 }
