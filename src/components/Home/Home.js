e24cdd26103fbbfcc165b72d181038aac8f97c39
jonasaky <luis.garcia@wizeline.com>
2022-01-19 21:59:02 -0600

    add custom welcome with user firstname

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,15 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import data from '../../data.json';
 import './Home.css';
+import { useUserContext } from '../../userContext';
 
 function Home() {
+  const context = useUserContext();
+  const { state: { user } } = context;
   return (
     <>
       <main>
-        <h2>Welcome to the Soap Store!</h2>
+        {user?.isLogged ? <h2>Welcome {user?.first}!</h2>  : <h2>Welcome to the Soap Store!</h2>}
         <p>You will find a whole bubble new experience.</p>
         <Box className='box-container'>
             <ImageList variant="masonry" cols={3} gap={8}>
